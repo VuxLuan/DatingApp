9ffa77cfe618f491b30b1a612661269c18ff3a32
VuxLuan <vuxluan@gmail.com>
2024-08-27 10:36:20 +0700

    End of section 7

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace API.Errors;
+
+public class ApiException(int stattusCode, string message, string? details)
+{
+    public int StattusCode { get; set; } =stattusCode;
+    public string Message { get; set; } = message;
+    public string? Details { get; set; } = details;
+}
